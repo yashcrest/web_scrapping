87ae5add0c9d328501256acb0e2f71233e027e7e
Yash Shrestha <yashcrest@gmail.com>
2024-03-10 20:07:58 +1100

    using beautifulSoup

--- scraper.py
+++ scraper.py
@@ -1,1 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
 
+URL = "https://realpython.github.io/fake-jobs/"
+indeedURL = "https://au.indeed.com/?r=us"
+authURL = "https://auth0.com/docs/authenticate/protocols/openid-connect-protocol"
+page = requests.get(URL)
+# print(page.text)
+# print(page.cookies)
+# print(page.raw)
+soup = BeautifulSoup(page.content, "html.parser")

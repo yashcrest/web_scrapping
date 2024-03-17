68f249c137f8d15e6762e6a4fe5afa52487004fd
Yash Shrestha <yashcrest@gmail.com>
2024-03-17 11:09:50 +1100

    refactoring, adding script to scrap for python jobs

--- scraper.py
+++ scraper.py
@@ -33,14 +33,19 @@ job_elements = results.find_all("div", class_="card-content")
 
 # filtering out developer jobs
 developer_jobs = results.find_all(
-    "h2", string=lambda text: "developer" in text.lower()
+    "h2", string=lambda text: "developer" in text.lower() # here, we are passing lambda function as str. It converts the string to lowercase.
 )
-for developer_job in developer_jobs:
-    job_title = developer_job.find("h2", class_="title")
-    company_title = developer_job.find("h3", class_="company")
-    location_title = developer_job.find("h2", class_="location")
-    print(job_title.text.strip())
-    print(company_title.text.strip())
-    print(location_title.text.strip())
-    print()
 
+# stepping over the DOM ladder to get parent element of h2 which contains the Developer job title
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in developer_jobs
+]
+
+for job in python_job_elements:
+    job_title = job.find('h2', class_="title")
+    location_title = job.find('p', class_="location")
+    company_title = job.find('h3', class_="company")
+    print(job_title.text)
+    print(location_title.text.strip())
+    print(company_title.text.strip())
+    print()
